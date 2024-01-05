02d3973e1a463bb570d88206f41c10f31aedbf22
rohit_reddy <rohithreddyedulla@gmail.com>
2024-01-05 13:54:47 +0530

    Update App.jsx

--- react_chat_app/src/App.jsx
+++ react_chat_app/src/App.jsx
@@ -2,13 +2,36 @@ import React from 'react'
 import Room from './pages/Room'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignUpPage'
-import 
-
+import { useState } from 'react'
+import { authService } from './appwrite/auth'
+import { login } from './store/authSlice'
+//import { useDispatch } from '@reduxjs/toolkit'
 function App() {
+
+  // const [loading , setLoading] = useState(true);
+  // const dispatch = useDispatch();
+
+
+  // useEffect(()=>{
+  //   authService.getCurrentUser()
+  //   .then((userData)=>{
+  //     if(userData){
+  //       dispatch(login({userData}))
+  //     }
+  //     else{
+  //       dispatch(logout())
+  //     }
+  //   })
+  //   .finally(()=>setLoading(false))
+  // },[])
+
   return (
-   
-   <LoginPage/>
-  )
-}
+     
+    <Room/>
+    
+    
+    
+  ) 
+  }
 
 export default App
